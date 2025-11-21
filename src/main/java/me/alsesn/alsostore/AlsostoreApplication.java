2e260473df51185642dce89f7ddbb751eacd6a3b
rosti <rostislavsusak747@gmail.com>
2025-11-21 14:59:57 +0100

    Initial commit

---
+++ src/main/java/me/alsesn/alsostore/AlsostoreApplication.java
@@ -0,0 +1,11 @@
+package me.alsesn.alsostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlsostoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AlsostoreApplication.class, args);
+	}
+}
---
+++ src/test/java/me/alsesn/alsostore/AlsostoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alsesn.alsostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlsostoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
